a4cad580e0d0cdede0c20a7bda03e580771502d2
Tommy <tommyzhangchina@gmail.com>
2020-06-16 22:23:47 +0800

    First Upload

---
+++ YangSpin/CardView.swift
@@ -0,0 +1,29 @@
+//
+//  CardView.swift
+//  YangSpin
+//
+//  Created by ZhangTommy on 16/6/20.
+//  Copyright © 2020 Tommy Zhang. All rights reserved.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    @Binding var symbol:Int
+    @Binding var bg:Color
+    var body: some View {
+        Image("p\(symbol)")
+        .resizable()
+        .aspectRatio( 1,contentMode: .fit)
+        .cornerRadius(20)
+        .padding(10)
+        .background(bg.opacity(0.5))
+        .cornerRadius(20)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView(symbol: Binding.constant(0),bg: Binding.constant(Color.green))
+    }
+}
---
+++ YangSpin/ContentView.swift
@@ -0,0 +1,75 @@
+//
+//  ContentView.swift
+//  YangSpin
+//
+//  Created by ZhangTommy on 16/6/20.
+//  Copyright © 2020 Tommy Zhang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var money = 100
+    @State private var num = [0,2,1]
+    @State private var bg = [Color.white, Color.white, Color.white]
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(Color(red: 110/255, green: 175/255, blue: 210/255))
+                .edgesIgnoringSafeArea(.all)
+            
+            Rectangle()
+                .foregroundColor(Color(red: 155/255, green: 205/255, blue: 220/255))
+                .rotationEffect(Angle(degrees: 45))
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Spacer()
+                HStack {
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.yellow)
+                    Text("YANG SPIN")
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.white)
+                    
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.yellow)
+                }.scaleEffect(2).shadow(radius: /*@START_MENU_TOKEN@*/5/*@END_MENU_TOKEN@*/)
+                Spacer()
+                Text("Money: \(money)")
+                    .padding(10)
+                    .background(Color.white.opacity(0.5))
+                    .cornerRadius(20)
+                Spacer()
+                HStack{
+                    Spacer()
+                    CardView(symbol: $num[0],bg: $bg[0])
+                    CardView(symbol: $num[1],bg: $bg[1])
+                    CardView(symbol: $num[2],bg: $bg[2])
+                    Spacer()
+                }
+                Spacer()
+                HStack{
+                    Button(action: {
+                        self.money -= 15
+                        
+                    }) {
+                        Text("Spin")
+                            .foregroundColor(.white)
+                            .padding(15)
+                            .padding(.horizontal, 30.0)
+                            .background(Color(.orange).opacity(0.8))
+                            .cornerRadius(20)
+                    }
+                }
+                Spacer()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
